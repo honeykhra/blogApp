b69aeae69a2ed5860a8c8f35c8e5b5d0bb18d4c2
honeykhra <honeykhra@gmail.com>
2022-02-27 20:47:21 +0530

    specific post query

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -9,7 +9,7 @@ import {
   CommentsForm,
 } from '../../components'
 
-const PostDetails = () => {
+const PostDetails = ({ post }) => {
   return (
     <div className=" container mx-auto mb-8 px-10">
       <div className=" grid grid-cols-1 gap-12 lg:grid-cols-12">
@@ -33,9 +33,9 @@ const PostDetails = () => {
 export default PostDetails
 
 export async function getStaticProps({ params }) {
-  const posts = (await getPosts()) || []
+  const data = await getPostDetails(params.slug)
 
   return {
-    props: { posts },
+    props: { post: data },
   }
 }
