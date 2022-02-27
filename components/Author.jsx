52446c4614b7b8fc360a4bf4af897a369f9cef91
honeykhra <honeykhra@gmail.com>
2022-02-27 21:46:19 +0530

    author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <h2>Author</h2>
- )
+    <div className=" relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center">
+      <div className="absolute left-0 right-2 -top-14">
+        <img
+          src={author.photo.url}
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+        />
+      </div>
+      <h3 className="text-xl font-bold text-white ">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio}</p>
+    </div>
+  )
 }
 
 export default Author
