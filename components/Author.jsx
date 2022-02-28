df03c7b70f25cac49970bbf70c8fa96254214f29
honeykhra <honeykhra@gmail.com>
2022-02-28 18:24:45 +0530

    author complete comments start

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Author = ({ author }) => {
   return (
     <div className=" relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center">
       <div className="absolute left-0 right-2 -top-14">
-        <img
+        <Image
           src={author.photo.url}
           alt={author.name}
+          unoptimized
           height="100px"
           width="100px"
           className="rounded-full align-middle"
--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
-const Comments = () => {
-  return (
-    <div>Comments</div>
-  )
+const Comments = ({ slug }) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+  const nameEl = useRef()
+  const commentEl = useRef()
+  const emailEl = useRef()
+  const storeDataEl = useRef()
+
+  return <div>Comments</div>
 }
 
 export default Comments
