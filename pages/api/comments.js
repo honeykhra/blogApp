9bdfdb7bbcaeb64ce3e2513d958d0b2f0f3cee71
honeykhra <honeykhra@gmail.com>
2022-02-28 20:05:39 +0530

    query to add comment

---
+++ pages/api/comments.js
@@ -0,0 +1,30 @@
+import { GraphQLClient, gql } from 'graphql-request'
+
+const graphAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+
+export default function comments(req, res) {
+  const graphQLClient = new GraphQLClient(graphAPI, {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  })
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $slug: String!) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
+  `
+
+  const result = await graphQLClient.request(query, req.body)
+
+  return res.status(200).send(result)
+}
--- services/index.js
+++ services/index.js
@@ -127,3 +127,12 @@ export const getPostDetails = async (slug) => {
   // console.log(result.postsConnection.edges)
   return result.post
 }
+
+export const submitComment = async (obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    body: JSON.stringify(obj),
+  })
+
+  return result.json()
+}
