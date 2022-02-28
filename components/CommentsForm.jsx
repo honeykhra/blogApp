3c5b77744b2d31e9da392d9d458b6ef9b9529d1b
honeykhra <honeykhra@gmail.com>
2022-02-28 18:53:27 +0530

    comments form design done

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -10,14 +10,52 @@ const Comments = ({ slug }) => {
   const emailEl = useRef()
   const storeDataEl = useRef()
 
+  const handleCommentSubmission = () => {}
+
   return (
     <div className=" mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg ">
       <h3 className="mb-8 border-b pb-4 text-xl font-semibold">CommentsForm</h3>
-      <div className="grid grid-cols-1 gap-4 mb-4">
-        <textarea></textarea>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <textarea
+          ref={commentEl}
+          className="w-full rounded-lg bg-gray-200 p-4 text-gray-700 outline-none focus:ring-gray-200 "
+          placeholder="Comment"
+          name="comment"
+        />
       </div>
-      <div className="grid grid-cols-1 gap-4 mb-4"></div>
-      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <input
+          type="text"
+          ref={nameEl}
+          className="w-full rounded-lg bg-gray-200 px-4 py-2 text-gray-700 outline-none focus:ring-gray-200 "
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="w-full rounded-lg bg-gray-200 px-4 py-2 text-gray-700 outline-none focus:ring-gray-200 "
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are required.</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className="ease inline-block cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-lg text-white transition duration-500 hover:bg-indigo-900"
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-4 text-xl font-semibold text-green-600 ">
+            Comment submitted for review
+          </span>
+        )}
+      </div>
     </div>
   )
 }
