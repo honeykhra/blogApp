350c70db4baeb28dc431381c5ff42432242f71a3
honeykhra <honeykhra@gmail.com>
2022-02-28 18:30:18 +0530

    commentsform

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React from 'react'
 
-const Comments = ({ slug }) => {
-  const [error, setError] = useState(false)
-  const [localStorage, setLocalStorage] = useState(null)
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
-
-  const nameEl = useRef()
-  const commentEl = useRef()
-  const emailEl = useRef()
-  const storeDataEl = useRef()
-
-  return <div>Comments</div>
+const Comments = () => {
+  return (
+    <div>Comments</div>
+  )
 }
 
 export default Comments
--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
-const CommentsForm = () => {
+const Comments = ({ slug }) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+  const nameEl = useRef()
+  const commentEl = useRef()
+  const emailEl = useRef()
+  const storeDataEl = useRef()
+
   return (
-    <div>CommentsForm</div>
+    <div className=" mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg ">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">CommentsForm</h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea></textarea>
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+    </div>
   )
 }
 
-export default CommentsForm
+export default Comments
