7515db3b4191875f505a0060b1fae872ed23e214
honeykhra <honeykhra@gmail.com>
2022-03-01 12:54:10 +0530

    comments start working

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
 
-const Comments = () => {
+const Comments = ({ slug }) => {
+  
+  
   return (
     <div>Comments</div>
   )
