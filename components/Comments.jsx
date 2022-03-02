d4e1b6f743c5d92dfe89105ef5b4ac9711665120
honeykhra <honeykhra@gmail.com>
2022-03-02 15:47:49 +0530

    comments show done

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -2,15 +2,41 @@ import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 import parse from 'html-react-parser'
 
+import { getComments } from '../services'
+
 const Comments = ({ slug }) => {
   const [comments, setComments] = useState([])
 
   useEffect(() => {
     //getComments(slug)
-    
+    getComments(slug).then((result) => setComments(result))
   }, [])
 
-  return <div>Comments</div>
+  return (
+    <>
+      {comments.length > 0 && (
+        <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg ">
+          <h3 className="mb-8 border-b pb-4 text-xl font-semibold ">
+            {comments.length} Comments
+          </h3>
+          {comments.map((comment) => (
+            <div
+              className="mb-4 border-b border-gray-100 pb-4"
+              key={comment.createdAt}
+            >
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span> on{' '}
+                {moment(comment.createdAt).format('MMM DD,YYYY')}
+              </p>
+              <p className=" w-full whitespace-pre-line text-gray-600">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Comments
