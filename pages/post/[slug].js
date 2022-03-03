de44af8f52fef0116d5ba6e32a2b0b1801990ff6
honeykhra <honeykhra@gmail.com>
2022-03-03 20:33:48 +0530

    final and ready for deploy

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+
 import { getPosts, getPostDetails } from '../../services'
 import {
   PostDetail,
@@ -7,11 +9,16 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from '../../components'
 
 const PostDetails = ({ post }) => {
   // console.log(post)
+  const router = useRouter()
 
+  if (router.isFallback) {
+    return <Loader />
+  }
   return (
     <div className=" container mx-auto mb-8 px-10">
       <div className=" grid grid-cols-1 gap-12 lg:grid-cols-12">
@@ -50,6 +57,6 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   }
 }
